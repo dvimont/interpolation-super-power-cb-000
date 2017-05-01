0231637cd2ce9c2aaf82d2232ea6eb36689e5162
Daniel Vimont <daniel.vimont@gmail.com>
2017-05-01 04:13:14 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,17 @@
 # Write your #display_rainbow method here
+def display_rainbow(input)
+  comma = String.new
+  input.each{
+    |color|
+    print(comma + color[0].upcase + ": #{color}")
+    comma = ", "
+  }
+  print"\n"
+end
+
+=begin
+colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+display_rainbow(colors)
+puts(colors)
+puts(colors.[](0))
+=end
